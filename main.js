527e50b50dfa72ad460afced734c50ebe2155e60
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-17 16:43:55 -0600

    Display input items on dom with attempts at persistance

--- main.js
+++ main.js
@@ -8,12 +8,44 @@ var doughnutSection = document.querySelector('.section-doughnuts');
 
 grabBtn.addEventListener('click', makeNewDoughnut)
 
-getDoughnuts();
+getDoughnutsFromStorage()
+displayDoughnuts()
 
 function makeNewDoughnut(e) {
   e.preventDefault();
   var doughnut = new Doughnut(Date.now(), typeInput.value, fillingInput.value, toppingInput.value, false, 0);
   doughnutsArray.push(doughnut);
-  console.log('array', doughnutsArray)
   doughnut.saveToStorage(doughnutsArray);
+  displayDoughnuts()
+  clearFormInputs()
 }
+
+function clearFormInputs() {
+  typeInput.value = "";
+  fillingInput.value = "";
+  toppingInput.value = "";
+}
+
+function getDoughnutsFromStorage() {
+  if (JSON.parse(localStorage.getItem('storedDoughnuts'))) {
+  } else {
+    doughnutsArray = JSON.parse(localStorage.getItem('storedDoughnuts')).map(function({id, type, filling, topping, star, quality}) {
+      return new Doughnut(id, type, filling, topping, star, quality);
+      });
+  }
+}
+
+function displayDoughnuts() {
+  for (var i = 0; i < doughnutsArray.length; i++) {
+    generateDoughnut(doughnutsArray[i]);
+  }
+}
+
+function generateDoughnut({id, type, filling, topping, star, quality}) {
+  doughnutSection.insertAdjacentHTML ('afterbegin',
+ `<article class="card-doughnut" data-id=${id}>
+    <h1>${type}</h1>
+    <h1>${filling}</h1>
+    <h1>${topping}</h1>
+  </article>`)
+};
