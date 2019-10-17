296f0fb352de26dc47e1211ad6223c373c92ad2e
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-17 14:54:29 -0600

    Add normalize text and basic css and js selectors

---
+++ main.js
@@ -0,0 +1,8 @@
+var doughnutsArray = [];
+
+var typeInput = document.querySelector('.input-type');
+var fillingInput = document.querySelector('.input-filling');
+var toppingInput = document.querySelector('.input-topping');
+var doughnutSection = document.querySelector('.section-doughnuts');
+
+doughnutSection.addEventListener('click', );
